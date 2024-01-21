b86a230e74d04a86a39d2790ecfabc9e653e024c
Mauro <mauroeavellaneda@gmail.com>
2024-01-21 21:27:01 +0100

    feat: add article struct

--- apps/backend/main.go
+++ apps/backend/main.go
@@ -6,6 +6,20 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Article struct {
+    ID          int       `json:"id"`
+    Title       string    `json:"title"`
+    Content     string    `json:"content"`
+    Author      string    `json:"author"`
+    // CreatedAt   time.Time `json:"created_at"`
+    // UpdatedAt   time.Time `json:"updated_at"`
+    // PublishedAt time.Time `json:"published_at"`
+    // Tags        []string  `json:"tags"`
+    // Status      string    `json:"status"` 
+    
+}
+
+
 func main() {
 
 	app := fiber.New()
