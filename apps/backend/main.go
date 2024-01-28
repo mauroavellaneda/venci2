522ffd4d55a96e6ad9e04216a008b4e966df043a
Mauro <mauroeavellaneda@gmail.com>
2024-01-28 17:48:12 +0100

    adds PUT method

--- apps/backend/main.go
+++ apps/backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -81,6 +82,39 @@ func main() {
 				break
 			}
 		}
+
+		return c.JSON(articles)
+	})
+
+	app.Put("/api/articles/:id", func(c *fiber.Ctx) error {
+		idParam := c.Params("id")
+		id, err := strconv.Atoi(idParam)
+		if err != nil {
+			return c.Status(400).SendString(err.Error())
+		}
+
+		var articleIndex int
+		found := false
+		for i, article := range articles {
+			if article.ID == id {
+				articleIndex = i
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			return c.Status(404).SendString("article not found")
+		}
+
+		updatedArticle := new(Article)
+		if err := c.BodyParser(updatedArticle); err != nil {
+			return c.Status(400).SendString(err.Error())
+		}
+
+		updatedArticle.ID = id
+
+		articles[articleIndex] = *updatedArticle
 
 		return c.JSON(articles)
 	})
