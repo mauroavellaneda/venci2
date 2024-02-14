e13b66829c92cbad790e81b84b5e148a3723a842
Mauro <mauroeavellaneda@gmail.com>
2024-02-14 16:47:28 +0100

    Update AddArticle.tsx

--- apps/frontend/src/app/components/articles/AddArticle.tsx
+++ apps/frontend/src/app/components/articles/AddArticle.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, ReactElement } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import styled from 'styled-components';
 import * as yup from 'yup';
@@ -24,7 +24,7 @@ const schema = yup
   })
   .required();
 
-const AddArticle = (props: AddArticleProps): void => {
+const AddArticle = (props: AddArticleProps): ReactElement => {
   const {
     register,
     handleSubmit,
