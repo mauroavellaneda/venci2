439d43c47916733335069fa4049e016523513903
Mauro <mauroeavellaneda@gmail.com>
2024-02-14 11:04:39 +0100

    add eslint to root

--- apps/frontend/src/app/components/articles/AddArticle.tsx
+++ apps/frontend/src/app/components/articles/AddArticle.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import styled from 'styled-components';
 import * as yup from 'yup';
